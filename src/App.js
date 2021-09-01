fb6bd16f469e1238542529e0c23b72f699d56dc3
emilioCode <emilio_mem@hotmail.com>
2021-09-01 00:06:43 -0400

    My first component including css and images

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react'
-
+import Card from './components/Card';
 class App extends React.Component{
   
   render(){
     return (
-      <div>
-        <h1>App works!</h1>
-        <p>This app  runs with Reactjs.</p>
-    </div>
+    //   <div>
+    //     <h1>App works!</h1>
+    //     <p>This app  runs with Reactjs.</p>
+    //     <Card/>
+    // </div>
+    <Card/>
     )
   }
 }
---
+++ src/components/Card.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import exerciseImg from './images/exercise.png';
+import './styles/Card.css';
+
+class Card extends React.Component{
+    render(){
+        return (
+           <div className="card mx-auto Fitness-Card">
+               <div className="card-body">
+                   <div className="row center">
+                        <div className="col-6">
+                            <img src={exerciseImg} className="float-right"/>
+                        </div>
+                        <div className="col-6 Fitness-Card-Info">
+                            <h1>Technique Guides</h1>
+                            <p>Learn amazing street workout and calisthenics</p>
+                        </div>
+                    </div>
+                </div>
+           </div>
+        )
+    }
+}
+
+export default Card;
