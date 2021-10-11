6fe80148f9c00e257f9c797ffb2b0ff46e20637f
emilioCode <emilio_mem@hotmail.com>
2021-10-11 12:53:01 -0400

    controlled component and values into state

--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -2,15 +2,50 @@ import React from 'react'
 
 class ExerciseNew extends React.Component{
 
-
-    handleClick=()=>{
-        console.log(this.props)
+    state = {}
+    handleSubmit= e =>{
+        e.preventDefault()
+        console.log(this.state)
+    }
+    handleChange=(e)=>{
+        // console.log(e.target.name + ": "+e.target.value)
+        let partialState = {}
+        partialState[e.target.name]= e.target.value
+        this.setState(partialState)
     }
     render(){
         return(
-            <button onClick={this.handleClick}>
-                Send
+            <div className="container">
+                <form
+                onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="title" name="title"
+                         value={this.state.title} onChange={this.handleChange}></input>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="description" name="description"
+                         value={this.state.description} onChange={this.handleChange}></input>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="img" name="img"
+                         value={this.state.img} onChange={this.handleChange}></input>
+                    </div>
+                    <div className="form-row">
+                        <div className="col">
+                            <input type="text" className="form-control" placeholder="leftColor" name="leftColor"
+                             value={this.state.leftColor} onChange={this.handleChange}></input>
+                        </div>
+                        <div className="col"> 
+                            <input type="text" className="form-control" placeholder="rightColor" name="rightColor"
+                             value={this.state.rightColor} onChange={this.handleChange}></input>
+                        </div>
+                    </div>
+
+            <button type="submit" className="btn btn-primary float-right">
+                Submit
             </button>
+                </form>
+            </div>
         )
     }
 }
